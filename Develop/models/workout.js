2f97acbd28f46558ecdd14d88e7fe513cedbfb2e
Reggie Littleton <Hitsupod@gmail.com>
2020-08-15 16:46:25 -0500

    Workout Schema creation

---
+++ Develop/models/workout.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Workout Schema Creation
+const workoutSchema = new Schema ({
+
+    
+})
+
+
+
+// Export
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;

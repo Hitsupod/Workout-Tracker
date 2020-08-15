efa7fc5dea08adbadc9066c548a8f43c3f3fa140
Reggie Littleton <Hitsupod@gmail.com>
2020-08-15 17:12:31 -0500

    Workout schema complete

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -3,10 +3,40 @@ const Schema = mongoose.Schema;
 
 // Workout Schema Creation
 const workoutSchema = new Schema ({
+    day:{
+        type: Date,
+        default: Date.now
+    },
+    exercise: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter the exercise name"
+          },
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter the exercise type"
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number,
+            required: "Enter the exercise duration"
+        },
+        distance: {
+            type: Number
+        }
+    }]
 
-    
 })
-
 
 
 // Export

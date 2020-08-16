4b2c74623112d5f1336d07457032ed52fa07d886
Reggie Littleton <Hitsupod@gmail.com>
2020-08-15 21:51:51 -0500

    Pathing Fix

--- routes/routes.js
+++ routes/routes.js
@@ -7,15 +7,15 @@ const path = require("path");
 
 // Page Routes 
 router.get("/", function(req, res){
-    res.sendFile(path.join(__dirname + "/public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
 router.get("stats", function(req, res){
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
+    res.sendFile(path.join(__dirname, "/public/stats.html"));
 })
 
 router.get("exercise", function(req, res){
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
 })
 
 

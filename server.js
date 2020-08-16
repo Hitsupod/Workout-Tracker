6d0b8cd4d12ae47805a20e68d6fca95318c47b65
Reggie Littleton <Hitsupod@gmail.com>
2020-08-15 22:07:05 -0500

    Test fix

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
-
+const db = require("./models")
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(require("./routes/routes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nosql", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("../Workout-Tracker/routes/routes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
